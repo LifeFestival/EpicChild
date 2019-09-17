e96bf53d2f836ecd567968f868bb15f38280acf3
Nick Treskin <PzFireFly@gmail.com>
2019-09-17 15:11:12 +0300

    Small Initial Changes

--- app/src/main/java/com/example/epicchild/EpicListActivity.kt
+++ app/src/main/java/com/example/epicchild/EpicListActivity.kt
@@ -3,10 +3,10 @@ package com.example.epicchild
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class EpicListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_epic_list)
     }
 }
